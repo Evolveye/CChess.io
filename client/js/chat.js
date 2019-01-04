0537d723ce6bd7bd8b46fb45cc9bcb3878423ff0
PawelekS <stolarski.paw@gmail.com>
2019-01-04 18:55:50 +0100

    ^ html modules

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,16 +1,24 @@
 import ws from "./ws.js"
 
-const chatMessages = document.querySelector( `.chat-messages` )
-const chatInput = document.querySelector( `.chat-input` )
+const chatbox = document.querySelector( `chat` )
+chatbox.innerHTML = /* html */ `
+  <div class="chat-messages">
+    <p>Hello 🌵</p>
+  </div>
+  <input class="chat-input">
+`
+
+const chatMessages = chatbox.querySelector( `.chat-messages` )
+const chatInput = chatbox.querySelector( `.chat-input` )
 
 chatInput.onkeydown = e => {
   if ( e.key === `Enter` && chatInput.value) {
-    ws.send( `chat_newMsg`, chatInput.value )
+    ws.send( `chat-new_message`, chatInput.value )
     chatInput.value = ``
   }
 }
 
-ws.on( `chat_newMsg`, msg => {
+ws.on( `chat-new_message`, msg => {
   const message = document.createElement( `p` )
   message.textContent = msg
   chatMessages.insertAdjacentElement( `beforeend`, message )
