e88cf66f1a39e1bab355fc3f9f804a27d4d33947
PawelekS <stolarski.paw@gmail.com>
2019-02-04 20:09:27 +0100

    + nickname cookie

--- client/js/main.js
+++ client/js/main.js
@@ -3,6 +3,10 @@ import ws from "./ws.js"
 
 const nicknameInput = document.querySelector( `.lobby input` )
 nicknameInput.focus()
+
+if ( document.cookie )
+  nicknameInput.value = document.cookie.split( `=` )[ 1 ]
+
 nicknameInput.onkeydown = e => {
   if ( e.keyCode != 13 )
     return
@@ -15,7 +19,8 @@ nicknameInput.onkeydown = e => {
 ws.on( `game-nickname`, good => {
   if ( good ) {
     document.querySelector( `.lobby` ).hidden = true
-    window.game = new Game( nicknameInput.value )
+    new Game( nicknameInput.value )
+    document.cookie = `nickname=${nicknameInput.value}`
   }
   else
     nicknameInput.disabled = false
